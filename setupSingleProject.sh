8aacb0db5897871976cb8a17977a68ba2041eaf3
McJty <mcjty1@gmail.com>
2018-05-07 20:48:24 +0200

    First commit

---
+++ setupSingleProject.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/McJty/RFTools.git
+git clone https://github.com/McJty/RFToolsControl.git
+git clone https://github.com/McJty/RFToolsPower.git
+git clone https://github.com/McJty/RFToolsDimensions.git
+git clone https://github.com/McJty/McJtyLib.git
+git clone https://github.com/McJty/XNet.git
+git clone https://github.com/McJty/Nice.git
+git clone https://github.com/McJty/ImmersiveCraft.git
+git clone https://github.com/McJty/AquaMunda.git
+git clone https://github.com/McJty/needtobreath.git NeedToBreathe
+git clone https://github.com/McJty/restrictions.git Restrictions
+git clone https://github.com/McJty/LostCities.git
+git clone https://github.com/McJty/DeepResonance.git
+git clone https://github.com/McJty/EFab.git
+git clone https://github.com/McJty/GearSwapper1.8.git GearSwapper
+git clone https://github.com/McJty/QuestUtils.git
+git clone https://github.com/McJty/MeeCreeps.git
+git clone https://github.com/romelo333/notenoughwands1.8.8.git NotEnoughWands
+git clone https://github.com/romelo333/rflux.git RFLux
+
+
+
